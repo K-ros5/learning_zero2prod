5473e4af7a79081032e4997473a1c8358daf9a55
K-ros5 <6686445+K-ros5@users.noreply.github.com>
2024-07-01 00:31:19 -0400

    Add source up to chapter 3.8.5.5
    implemented a http server with a health_check endpoint and the beginning
    of a subscription endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use learning_zero2prod::{configuration::get_configuration, startup::run};
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = TcpListener::bind(address)?;
+
+    run(listener)?.await
 }
